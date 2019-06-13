e2d4dad6032b3e28cb1e444685681462a03cca3b
Daniel Way <projects@danieldway.com>
2019-06-12 21:38:16 -0500

    Changes to build process to support CJS libraries

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
 
 export default {
     input: 'src/ts-cache.ts',
@@ -7,5 +9,5 @@ export default {
         { file: 'dist/ts-cache.es5.js', format: 'es', sourcemap: true }
     ],
     watch: { include: 'src/**' },
-    plugins: [ typescript({ useTsconfigDeclarationDir: true }) ]
+    plugins: [ typescript({ useTsconfigDeclarationDir: true }), commonjs(), resolve() ]
 }
